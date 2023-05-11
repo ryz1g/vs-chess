3dc990053a191f774e0321cf8bba6e759b5f3c9d
Mohammad Saif <mohd.saif98@gmail.com>
2023-05-11 22:40:46 +0530

    Added assets and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
+import './App.css'
+
+const Pos = styled.div`
+    position: absolute;
+    top: 50px;
+    left: 250px;
+`
 
 function App() {
-  return (
-    <div className="App">
-      <div> Hello Chess </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <img
+                src={process.env.PUBLIC_URL + '/assets/board.jpg'}
+                alt="img"
+                width="100%"
+            />
+            <Pos>
+                <img
+                    src={process.env.PUBLIC_URL + '/assets/pieces/w_king.png'}
+                    alt="img"
+                    height="100px"
+                    width="50px"
+                />
+            </Pos>
+        </div>
+    )
 }
 
-export default App;
+export default App
